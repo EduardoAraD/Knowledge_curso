47940f243d69f7ae9c54b0363c8cb99d5086dc7f
EduardoAraD <araujocarlos893@gmail.com>
2019-10-23 17:09:36 -0300

    Criando templates do sistema

---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="footer">
+        <span>Copyrigth <strong>Cod3r</strong> © 2015-2019</span>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #DDD;
+        color: #333;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+</style>
